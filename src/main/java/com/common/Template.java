bbd7ed349f349401baf3d4beb5c023cbb3ef1d89
wjs960 <wjs960@gmail.com>
2023-08-11 19:42:13 +0900

    2023-08-11 집에가자

--- src/main/java/com/common/Template.java
+++ src/main/java/com/common/Template.java
@@ -2,6 +2,7 @@ package com.common;
 
 import com.management.product.model.dao.ProductDAO;
 import org.apache.ibatis.datasource.pooled.PooledDataSource;
+import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.mapping.Environment;
 import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSession;
@@ -9,24 +10,25 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Template {
-    private static String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static String URL = "jdbc:mysql://localhost/product";
-    private static String USER= "ohgiraffers";
-    private static String PASSWORD= "ohgiraffers";
 
     private static SqlSessionFactory sqlSessionFactory;
     public static SqlSession getSqlSession(){
         if(sqlSessionFactory == null){
-            Environment environment = new Environment("dev",
-                    new JdbcTransactionFactory(),
-                    new PooledDataSource(DRIVER,URL,USER,PASSWORD));
+            String resource = "config/mybatis-config.xml";
 
-            Configuration configuration = new Configuration();
-            configuration.addMapper(ProductDAO.class);
-            sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
+            try {
+                InputStream inputStream = Resources.getResourceAsStream(resource);
+                sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
-        System.out.println("연결성공");
         return sqlSessionFactory.openSession(false);
     }
 
--- src/main/java/com/management/product/model/dao/ProductDAO.java
+++ src/main/java/com/management/product/model/dao/ProductDAO.java
@@ -1,13 +1,22 @@
 package com.management.product.model.dao;
 
+import com.common.SearchCondition;
 import com.management.product.model.dto.ProductDTO;
 
 import java.util.List;
+import java.util.Map;
 
 public interface ProductDAO {
     List<ProductDTO> selectAllProductList();
 
+    List<ProductDTO> selectProductByCondition(SearchCondition searchCondition);
 
-    // * 주석을 지우고 Mapper interface(= DAO class) 역할을 하도록 내용을 작성하세요.
+    int registNewProduct(ProductDTO product);
+
+    int modifyProductInfo(ProductDTO product);
+
+    int deleteProduct(Map<String, String> parameter);
+
+
 
 }
