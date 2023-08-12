64a1b757480cae126ee021928e5a94fe90a08dff
Gray-Grazer <cleit8307@gmail.com>
2023-08-13 03:52:51 +0900

    20230812

--- src/main/java/com/management/product/view/ProductMenu.java
+++ src/main/java/com/management/product/view/ProductMenu.java
@@ -52,7 +52,7 @@ public class ProductMenu {
         System.out.println("===================================");
         System.out.println("1. 제품명으로 조회");
         System.out.println("2. 판매점별 취급 제품 조회");
-        System.out.println("3. 이달의 신재품 조회");
+        System.out.println("3. 이달의 신 제품 조회");
         System.out.println("4. 생산 중단 제품 조회");
         System.out.println("===================================");
         System.out.println("원하는 조건의 번호를 입력해 주세요 : ");
@@ -82,8 +82,8 @@ public class ProductMenu {
             }
 
         SearchCondition searchCondition = new SearchCondition();
-        // 주석을 지우고 searchCondition 검색조건과 검색어를 searchCondition 객체에 setting 하세요.
-
+        searchCondition.setSearchOption(searchOption);
+        searchCondition.setSearchValue(searchValue);
         return searchCondition;
     }
 
@@ -111,7 +111,7 @@ public class ProductMenu {
         String productCode = sc.nextLine();
 
         ProductDTO productDTO = new ProductDTO();
-        // 주석을 지우고 받아온 제품 코드를 productDTO 객체에 setting 하세요.
+        productDTO.setProductCode(productCode);
 
         getProductInfo(productDTO);
 
@@ -119,8 +119,8 @@ public class ProductMenu {
         String salesQuantity  = sc.nextLine();
         System.out.println("제품의 생산여부를 입력해 주세요(Y:생산중 / H:생산보류 / N:생산중단) : ");
         String productionStatus = sc.nextLine().toUpperCase();
-
-        // 주석을 지우고 받아온 활동 상태를 productDTO 객체에 setting 하세요.
+        productDTO.setSalesQuantity(salesQuantity);
+        productDTO.setProductionStatus(productionStatus);
 
         System.out.println("===================================");
 
@@ -143,7 +143,12 @@ public class ProductMenu {
         System.out.println("제품의 할인율을 입력해 주세요 : ");
         String discountRate  = sc.nextLine();
 
-        // 주석을 지우고 받아온 정보들을 productDTO 객체에 setting 하세요.
+        productDTO.setProductName(productName);
+        productDTO.setCategoryCode(categoryCode);
+        productDTO.setOriginCost(originCost);
+        productDTO.setReleaseDate(releaseDate);
+        productDTO.setStockQuantity(stockQuantity);
+        productDTO.setDiscountRate(discountRate);
 
         return productDTO;
     }
