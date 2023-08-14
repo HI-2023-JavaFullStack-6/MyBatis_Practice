54f73ea17663c474d7a7953cd64c78ca13fdeae4
somang1206 <dlthakd1206@naver.com>
2023-08-14 18:05:32 +0900

    소망 MyBatis과제 완료

--- src/main/java/com/management/product/model/service/ProductService.java
+++ src/main/java/com/management/product/model/service/ProductService.java
@@ -1,22 +1,35 @@
 package com.management.product.model.service;
 
 import com.common.SearchCondition;
+import com.management.product.model.dao.ProductDAO;
 import com.management.product.model.dto.ProductDTO;
+import org.apache.ibatis.session.SqlSession;
 
 import java.util.List;
 import java.util.Map;
+
+import static com.common.Template.getSqlSession;
 
 public class ProductService {
 
     // * 주석을 지우고 Service 역할에 해당하는 메소드를 작성하세요.
 
     // 1. 자주 사용할 DAO 객체를 선언s하세요.
+    private ProductDAO productDAO;
 
     public List<ProductDTO> selectAllProductList() {
 
         // 2. 전체 제품 목록을 조회하는 로직을 작성하세요.
         // 　　아래 작성된 return null은 과제 툴 오류를 제거하고자 임의 작성하였으니 지우고 로직을 작성하세요.
-        return null;
+
+        SqlSession sqlSession = getSqlSession();
+
+        productDAO = sqlSession.getMapper(ProductDAO.class);
+        List<ProductDTO> menuList = productDAO.selectAllProductList();
+
+        sqlSession.close();
+
+        return menuList;
 
     }
 
@@ -24,7 +37,14 @@ public class ProductService {
 
         // 3. 조건에 따른 제품 목록을 조회하는 로직을 작성하세요.
         // 　　아래 작성된 return null은 과제 툴 오류를 제거하고자 임의 작성하였으니 지우고 로직을 작성하세요.
-        return null;
+
+        SqlSession sqlSession = getSqlSession();
+
+        productDAO = sqlSession.getMapper(ProductDAO.class);
+        List<ProductDTO> menuList = productDAO.selectProductByCondition(searchCondition);
+
+        sqlSession.close();
+        return menuList;
 
     }
 
@@ -32,23 +52,65 @@ public class ProductService {
 
         // 4. 제품 정보를 등록하는 로직을 작성하세요.
         // 　　아래 작성된 return false 과제 툴 오류를 제거하고자 임의 작성하였으니 지우고 로직을 작성하세요.
-        return false;
+
+        SqlSession sqlSession = getSqlSession();
+
+        productDAO = sqlSession.getMapper(ProductDAO.class);
+
+        int result = productDAO.insertProduct(product);
+
+        if (result > 0) {
+            sqlSession.commit();
+        } else {
+            sqlSession.rollback();
+        }
+
+        sqlSession.close();
+
+        return result > 0 ? true : false;
 
     }
 
     public boolean modifyProductInfo(ProductDTO product) {
 
-        // 5. 제품 정보를 수정하는 로직을 작성하세요.
-        // 　　아래 작성된 return false 과제 툴 오류를 제거하고자 임의 작성하였으니 지우고 로직을 작성하세요.
-        return false;
 
+        SqlSession sqlSession = getSqlSession();
+
+        productDAO = sqlSession.getMapper(ProductDAO.class);
+        int result = productDAO.updateProduct(product);
+
+        if (result > 0) {
+            sqlSession.commit();
+        } else {
+            sqlSession.rollback();
+        }
+
+        sqlSession.close();
+
+
+        return result > 0 ? true : false;
     }
 
     public boolean deleteProduct(Map<String, String> parameter) {
 
-        // 6. 제품 정보를 삭제하는 로직을 작성하세요.
-        // 　　아래 작성된 return false 과제 툴 오류를 제거하고자 임의 작성하였으니 지우고 로직을 작성하세요.
-        return false;
+
+        SqlSession sqlSession = getSqlSession();
+
+        productDAO = sqlSession.getMapper(ProductDAO.class);
+
+        int result = productDAO.deleteProduct(parameter);
+
+        if( result > 0) {
+            sqlSession.commit();
+        }else{
+            sqlSession.rollback();
+        }
+
+        sqlSession.close();
+
+        return result > 0 ? true : false;
 
     }
+
+
 }
--- src/main/java/com/management/product/view/ProductPrint.java
+++ src/main/java/com/management/product/view/ProductPrint.java
@@ -6,37 +6,58 @@ import com.management.product.model.dto.ProductDTO;
 import java.util.List;
 
 public class ProductPrint {
+    private SearchCondition searchCondition;
 
-    // * 모든 출력은 System.out.println()을 통해 콘솔에 출력하는 것을 의미합니다.
-    // 주석을 지우고 다음 네 가지 메소드를 작성하세요.
 
     public void printAllProductList(List<ProductDTO> allProductList) {
 
-        // 1. 전체 조회한 목록 출력하는 메소드
-        //    (조건) List<ProductDTO>로 받아온 데이터 목록을 전체 출력하세요.
+
+
+        for(ProductDTO menu : allProductList) {
+            System.out.println(menu);
+        }
 
     }
 
-    public void printProductList(List<ProductDTO> productList, SearchCondition searchCondition) {
+    public void printProductList(List<ProductDTO> productList) {
 
-        // 2. 조건에 따라 조회한 목록을 출력하는 메소드
-        //    (조건 1) SearchCondition 객체로 검색 조건이 무엇인지 출력하세요.
-        //    (조건 2) List<ProductDTO>로 받아온 데이터 목록을 전체 출력하세요.
 
+
+        for(ProductDTO menu: productList) {
+            System.out.println(menu);
+        }
     }
 
     public void printSuccessMessage(String successCode) {
 
-        // 3. 성공메시지를 출력하는 메소드
-        //    (조건) 성공코드를 전달받아 성공을 알리는 메시지를 출력하세요.
 
+
+        String successMessage = "";
+
+        switch (successCode) {
+            case "insert" : successMessage = "신규 메뉴 등록에 성공했습니다."; break;
+            case "update" : successMessage = "메뉴 수정에 성공했습니다." ; break;
+            case "delete" : successMessage = "메뉴 삭제에 성공했습니다." ;break;
+
+        }
+        System.out.println(successMessage);
     }
 
     public void printErrorMessage(String errorCode) {
 
-        // 4. 에러메시지를 출력하는 메소드
-        //    (조건) 에러코드를 전달받아 에러를 알리는 메시지를 출력하세요.
 
+
+        String errorMessage = "";
+
+        switch (errorCode){
+            case "selectAllList" : errorMessage = "전체 메뉴 조회를 실패했습니다"; break;
+            case "selectProductByCondition" : errorMessage = "검색한 조건으로 메뉴 조회를 실패했습니다"; break;
+            case "insert" : errorMessage = "신규 메뉴 등록에 실패했습니다.";break;
+            case "update" : errorMessage = "메뉴 수정에 실패했습니다";break;
+            case "delete" : errorMessage = "메뉴 삭제에 실패했습니다."; break;
+        }
+
+        System.out.println(errorMessage);
     }
 
 }
